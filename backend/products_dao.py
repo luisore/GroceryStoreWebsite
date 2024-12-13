cd89e0b24f3de5cdb94d49cc11e160ec8f25298f
luisore <luissk47@gmail.com>
2024-12-13 16:41:24 -0300

    added insert new product

--- backend/products_dao.py
+++ backend/products_dao.py
@@ -6,16 +6,38 @@ def get_all_products(connection):
     cursor.execute(query)
     response = []
     for (product_id, name, uom_id, price_per_unit, uom_name) in cursor:
-        response.append({
-            'product_id': product_id,
-            'name': name,
-            'uom_id': uom_id,
-            'price_per_unit': price_per_unit,
-            'uom_name': uom_name
-        })
+        response.append(
+            {
+                'product_id': product_id,
+                'name': name,
+                'uom_id': uom_id,
+                'price_per_unit': price_per_unit,
+                'uom_name': uom_name
+            }
+        )
     return response
+
+
+def insert_new_product(connection, product):
+    cursor = connection.cursor()
+    query = ("INSERT INTO products "
+             "(name, uom_id, price_per_unit)"
+             "VALUES (%s,%s,%s)")
+
+    data = (product['product_name'],product['uom_id'],product['price_per_unit'])
+    cursor.execute(query,data)
+    connection.commit()
+
+    return cursor.lastrowid
+
 
 if __name__ == '__main__':
     connection = get_sql_connection()
-    print(get_all_products(connection))
-
+    #print(get_all_products(connection))
+    print(insert_new_product(connection, 
+        {
+            'product_name': 'cabbage',
+            'uom_id':'1',
+            'price_per_unit':'10'
+        }
+        ))

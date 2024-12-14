4300434c071187eceb4e02aa0674b610df7a2262
luisore <luissk47@gmail.com>
2024-12-13 21:06:43 -0300

    agrego delete

--- backend/products_dao.py
+++ backend/products_dao.py
@@ -31,13 +31,23 @@ def insert_new_product(connection, product):
     return cursor.lastrowid
 
 
+def delete_product(connection, product_id):
+    cursor = connection.cursor()
+    query=("DELETE FROM products where product_id="+str(product_id))
+    cursor.execute(query)
+    connection.commit()
+
+
+
 if __name__ == '__main__':
     connection = get_sql_connection()
     #print(get_all_products(connection))
-    print(insert_new_product(connection, 
+    """print(insert_new_product(connection, 
         {
             'product_name': 'cabbage',
             'uom_id':'1',
             'price_per_unit':'10'
         }
         ))
+    """
+    print(delete_product(connection,13))
